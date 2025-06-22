a66c46aac636a64ae06fac404b1577f4177da80b
Julian Bermudez <julianbetov@gmail.com>
2025-06-22 16:19:56 -0500

    Add HexColor annotation and validator for hex color format validation

---
+++ src/main/java/org/elara/app/validation/validation/validator/HexColorValidator.java
@@ -0,0 +1,17 @@
+package org.elara.app.validation.validation.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.elara.app.validation.validation.annotation.HexColor;
+
+public class HexColorValidator implements ConstraintValidator<HexColor, String> {
+
+    final static String HEX_COLOR_REGEX = "^(#([A-Fa-f0-9]{6}))";
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
+        if (value == null || value.isEmpty()) return true;
+        return value.matches(HEX_COLOR_REGEX);
+    }
+
+}
